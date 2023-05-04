cbd054831dd55f3a0230b439c9bdd2a1429615c2
Osiris Robledo <robledoosiris@gmail.com>
2023-05-04 23:36:10 +0000

    Hello World Todo app challenge

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['tasks' => Task::all()]);
 });
+
+Route::resource('tasks', \App\Http\Controllers\TaskController::class);
